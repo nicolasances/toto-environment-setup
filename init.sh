de945c94918720a72da73adc9fdbdfbe09919867
nicolas <nicolas.matteazzi@gmail.com>
2018-07-15 20:58:45 +0200

    Enable and start docker

--- init.sh
+++ init.sh
@@ -9,6 +9,10 @@ sudo yum install -y yum-utils device-mapper-persistent-data lvm2;
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo;
 sudo yum install -y docker-ce;
 
+# Enable and start docker
+systemctl enable docker;
+systemctl start docker;
+
 # Create the network for Toto
 docker network create totonet;
 
