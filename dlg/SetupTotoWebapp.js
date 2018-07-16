ab180f1127bf4b88dc84ce203e864f451e3f0cd1
nicolas <nicolas.matteazzi@gmail.com>
2018-07-16 18:30:42 +0200

    Deploy Toto webapp

--- dlg/SetupTotoWebapp.js
+++ dlg/SetupTotoWebapp.js
@@ -1,7 +1,26 @@
 
+var exec = require('child_process').exec;
+
 exports.do = function() {
 
   return new Promise(function(success, failure) {
+
+    // Create command
+    var command = '';
+
+    // Pull the image from
+    command += 'docker run -d --network totonet --name toto --restart always nicolasances/toto:latest';
+
+    exec(command, function(err, stdout, stderr) {
+
+      if (err != null) {
+        console.log('Could not deploy toto');
+        console.log(err);
+      }
+
+      success();
+
+    });
 
     success();
 
