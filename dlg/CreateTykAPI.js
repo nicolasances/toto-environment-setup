58c699bd1123ae88f876f65aafd087a6c8d6fb57
nicolas <nicolas.matteazzi@gmail.com>
2018-07-18 20:10:36 +0200

    Switch from .post() to .request()

--- dlg/CreateTykAPI.js
+++ dlg/CreateTykAPI.js
@@ -53,7 +53,7 @@ exports.do = function() {
     console.log('Creating Tyk API');
     console.log(data);
 
-    http.post(data, function(error, response, body) {
+    http.request(data, function(error, response, body) {
 
       console.log(body);
 
