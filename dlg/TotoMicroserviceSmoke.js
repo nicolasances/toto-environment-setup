dd1efeb40128d8cadd7c37de44e42a9ddf895ba1
nicolas <nicolas.matteazzi@gmail.com>
2018-08-04 18:55:24 +0200

    Normal smoke test utility

---
+++ dlg/TotoMicroserviceSmoke.js
@@ -0,0 +1,27 @@
+var smoke = require('./TotoMicroserviceSmoke');
+var http = require('request');
+
+exports.do = function(api) {
+
+  return new Promise(function(success, failure) {
+
+    // Create the http request to query the data
+    var req = {
+      url: 'http://' + api.localhost + ':8080/',
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json'
+      }
+    }
+
+    // Call the API and check the status
+    http(req, function(err, resp, body) {
+
+      if (err || body == null || JSON.parse(body).status != 'running') failure();
+      else success();
+
+    });
+
+  });
+
+}
