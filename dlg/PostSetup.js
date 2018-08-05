dfdbfed0f18d954b3d0b02765d188c20783f0fc8
nicolas <nicolas.matteazzi@gmail.com>
2018-08-05 12:46:40 +0200

    Docker hub user and pwd not necessary anymore... will be retrieved from env vars

--- dlg/PostSetup.js
+++ dlg/PostSetup.js
@@ -20,7 +20,6 @@ exports.do = function(conf) {
     // Check that the provided conf has the required data
     if (conf == null) {failure({status: 400, error: 'No configuration provided'}); return;}
     if (conf.env == null) {failure({status: 400, error: 'No "env" field provided in the configuration object. Please provide an env: "prod" or "dev"'}); return;}
-    if (conf.dockerhubUser == null || conf.dockerhubPwd == null) {failure({status: 400, error: 'No Docker Hub credentials passed as dockerhubUser and dockerhubPwd'}); return;}
     if (conf.host == null) {failure({status: 400, error: 'No \'host\' provided in the body'}); return;}
     if (conf.apiAuth == null) {failure({status: 400, error: 'No apiAuth data passed. Please provide an apiAuth object {user: <user>, pwd: <pswd>} to be used for API access.'}); return;}
 
@@ -67,7 +66,7 @@ exports.do = function(conf) {
         }, () => {failure({status: 500, completed: false, message: 'Toto Environment setup failed...'})});
 
       }, () => {failure({status: 500, completed: false, message: 'Toto Environment setup failed...'})});
-      
+
     }, () => {failure({status: 500, completed: false, message: 'Toto Environment setup failed...'})});
 
   });
