383ff6e61657d14d5f3dd0ee916a522fbff7df3b
nicolas <nicolas.matteazzi@gmail.com>
2018-08-05 15:26:33 +0200

    Integrated polling on CI release ms

--- dlg/TotoMicroserviceRelease.js
+++ dlg/TotoMicroserviceRelease.js
@@ -22,16 +22,33 @@ exports.do = function(api) {
     // Call the API
     http(req, (err, resp, body) => {
 
-      if (err || resp.statusCode == 500) {
-        if (err) console.error(err);
-        if (resp && resp.statusCode == 500) console.log(body);
-        failure({api: api.localhost, deployed: false, message: 'Couldn\'t release ' + api.localhost});
-        return;
-      }
+      // Starting the polling to check the status of the microservice deployment
+      var status = null;
+      while (status != 'RELEASED') {
 
-      if (resp.statusCode == 200) {
-        success({api: api.localhost, deployed: true});
-        return;
+        setTimeout(() => {
+
+          // Prepare the polling request
+          var r = {
+            url : 'http://toto-ci-release:8080/releases/' + api.localhost,
+            method : 'GET',
+            headers : {
+              'Accept' : 'application/json'
+            }
+          };
+
+          // Retrieve the status
+          http(r, (err, resp, body) => {
+
+            var response = JSON.parse(body);
+
+            if (response.status != status) console.log('[' + api.localhost + '] - ' + response.status);
+
+            if (body) status = response.status;
+
+          });
+
+        }, 1000);
       }
 
       success();
