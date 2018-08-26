63c8b1769eef098abe7c0dc770f2e37362565c75
nicolas <nicolas.matteazzi@gmail.com>
2018-08-26 15:02:57 +0200

    Added web apps in NGINX conf

--- dlg/NGINXConf.js
+++ dlg/NGINXConf.js
@@ -9,49 +9,68 @@ exports.do = function(conf) {
 
   return new Promise(function(success, failure) {
 
-    // Create the NGINX configuration data
-    var data = '';
+    //Retrieve the toto web apps
+    getGithubApis.getApis().then(function(data) {
 
-    // Create the basic server
-    data += 'http { \r\n';
-    data += '\t server { \r\n';
+      // Retrieve the toto web apps
+      var webapps = [];
 
-    // Create the SSL stuff
-    if (conf.ssl) {
-      data += '\t\t listen 443 ssl; \r\n';
-      data += '\t\t server_name ' + conf.host + '; \r\n';
-      data += '\t\t ssl_certificate /certificates/fullchain1.pem; \r\n';
-      data += '\t\t ssl_certificate_key /certificates/privkey1.pem; \r\n';
-    }
-
-    // Create the toto proxy pass
-    data += '\t\t location /toto/ { \r\n';
-    data += '\t\t\t proxy_pass http://toto/; \r\n';
-    data += '\t\t } \r\n';
-
-    // Create the gateway proxy pass
-    data += '\t\t location /apis/ { \r\n';
-    data += '\t\t\t proxy_pass http://gateway:8080/; \r\n';
-    data += '\t\t } \r\n';
-
-    // Close the basic http and server config
-    data += '\t } \r\n';
-    data += '} \r\n';
-
-    // Events tag
-    data += 'events {} \r\n';
-
-    // Create the file
-    fs.writeFile('/nginx-setup/nginx.conf', data, function(err, data) {
-
-      if (err) {
-        failure(err);
-        return;
+      for (var i = 0; i < data.apis.length; i++) {
+        if (data.apis[i].localhost.startsWith('toto-web-')) webapps.push(data.apis[i]);
       }
 
-      console.log("NGINX : nginx.conf file created!");
+      // Create the NGINX configuration data
+      var data = '';
 
-      success();
+      // Create the basic server
+      data += 'http { \r\n';
+      data += '\t server { \r\n';
+
+      // Create the SSL stuff
+      if (conf.ssl) {
+        data += '\t\t listen 443 ssl; \r\n';
+        data += '\t\t server_name ' + conf.host + '; \r\n';
+        data += '\t\t ssl_certificate /certificates/fullchain1.pem; \r\n';
+        data += '\t\t ssl_certificate_key /certificates/privkey1.pem; \r\n';
+      }
+
+      // Create the toto proxy pass
+      data += '\t\t location /toto/ { \r\n';
+      data += '\t\t\t proxy_pass http://toto/; \r\n';
+      data += '\t\t } \r\n';
+
+      // Create the gateway proxy pass
+      data += '\t\t location /apis/ { \r\n';
+      data += '\t\t\t proxy_pass http://gateway:8080/; \r\n';
+      data += '\t\t } \r\n';
+
+      // Create the proxy pass for web apps
+      for (var i = 0; i < webapps.length; i++) {
+        data += '\t\t location /' + webapps[i].name + '/ { \r\n';
+        data += '\t\t\t proxy_pass http://' + webapps[i].localhost + '/; \r\n';
+        data += '\t\t } \r\n';
+      }
+
+      // Close the basic http and server config
+      data += '\t } \r\n';
+      data += '} \r\n';
+
+      // Events tag
+      data += 'events {} \r\n';
+
+      // Create the file
+      fs.writeFile('/nginx-setup/nginx.conf', data, function(err, data) {
+
+        if (err) {
+          failure(err);
+          return;
+        }
+
+        console.log("NGINX : nginx.conf file created!");
+
+        success();
+
+      });
 
     });
 
