ab3ad2addabc7db36c2a2755389f6b7ce349b6be
nicolas <nicolas.matteazzi@gmail.com>
2019-01-12 16:57:55 +0100

    Release microservices one at a time, so that we don't fuck up tyk

--- dlg/SetupMicroservices.js
+++ dlg/SetupMicroservices.js
@@ -2,6 +2,7 @@
 var getGithubApis = require('./GetGithubApis');
 var releaseMs = require('./TotoMicroserviceRelease');
 
+
 exports.do = function(conf) {
 
   return new Promise(function(success, failure) {
@@ -13,33 +14,41 @@ exports.do = function(conf) {
 
       console.log("Toto Microservices : retreived " + data.apis.length + " Toto Microservices from Github.");
 
-      // 2. Build and deploy each api
+      // 2. Build and deploy each api, ONE AT A TIME
       // Save each promise and then join them
-      var buildPromises = [];
-
-      for (var i = 0; i < data.apis.length; i++) {
-
-        // Exclude toto-ci apis since they have already been deployed through init.sh
-        if (data.apis[i].type == 'toto-ci') continue;
-
-        console.log("Toto Microservices : deploying " + data.apis[i].localhost);
-
-        // Build and release the microservice
-        buildPromises.push(releaseMs.do(data.apis[i], conf));
-
-      }
-
-      // Wait for all the promises to finish
-      Promise.all(buildPromises).then(function() {
+      releaseNextAPI(data.apis, conf).then(() => {
 
         console.log("Toto Microservices : completed deploy of all microservices!");
 
         success();
 
-      }, failure);
+      }, () => {
+
+        console.log("Toto Microservices : FAILURE deploying some microservices!");
+
+        failure();
+
+      });
 
     });
 
   });
 
+}
+
+var releaseNextAPI = function(apis, conf) {
+
+  return new Promise(function(success, failure) {
+
+    // Terminating condition for the recursive function: no more apis to release
+    if (apis == null || apis.length == 0) success();
+
+    // Release the api
+    releaseMs.do(apis.pop(), conf).then(() = {
+
+      // Release next api
+      releaseNextAPI(apis, conf).then(success;
+    });
+
+  });
 }
